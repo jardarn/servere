0cc7c7e8625f14b4005b5cf659524012bc7b67ef
Marius Mandal <mariusmandal@gmail.com>
2020-10-22 18:34:19 +0200

    Filplasseringer

--- vagrant/filesystem.rb
+++ vagrant/filesystem.rb
@@ -1,15 +1,16 @@
 require 'fileutils'
 
 def requireServerDataFolder(relative_directory)
-    dirname = File.dirname($localNFSpath + '../server_data/'+ relative_directory +'/')
+    dirname = File.expand_path($localNFSpath + '../shared/'+ relative_directory +'/')
     unless File.directory?(dirname)
        FileUtils.mkdir_p(dirname)
     end
-    return true
+    return dirname
 end
 
 # Share folder between vm and host
 def share(box, localpath, serverpath)   
-    requireServerDataFolder(localpath)
-    box.vm.synced_folder $localNFSpath + "../server_data/" + localpath, serverpath, type: "nfs", create: true
+    localpath = requireServerDataFolder(localpath)
+    box.vm.synced_folder localpath, serverpath, type: "nfs", create: true
+    puts '    mounting point: '+ localpath +' => '+ serverpath
 end
