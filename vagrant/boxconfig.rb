63581f382142bfa54eadba50525fda5b544c2b76
Marius Mandal <mariusmandal@gmail.com>
2020-10-22 17:49:59 +0200

    dsstore er kjipe greier

--- vagrant/boxconfig.rb
+++ vagrant/boxconfig.rb
@@ -1,7 +1,7 @@
 $boxConf = {}
 
 Dir.foreach('./boxconfig') do |filename|
-    next if filename == '.' or filename == '..'
+    next if filename == '.' or filename == '..' or filename.include?('DS_Store')
     $boxNames << filename
     require "./boxconfig/#{filename}"
 end
