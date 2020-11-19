13c7e5ba7a2c9fae4ce7f3e91de556ae21af8bba
Temp <temp@ukm.no>
2020-11-19 17:46:43 +0100

    Sett nye IPer for å ikke få conflict med gammelt oppsett.

--- vagrant/boxconfig/lite.rb
+++ vagrant/boxconfig/lite.rb
@@ -1,6 +1,6 @@
 $boxConf['lite'] = {
     disksize: '20GB',
     memory: '4096',
-    ip: '10.0.10.9',
+    ip: '10.0.10.21',
     hostname: 'lite.ukm.dev'
 }
--- vagrant/boxconfig/main.rb
+++ vagrant/boxconfig/main.rb
@@ -1,6 +1,6 @@
 $boxConf['main'] = {
     disksize: '40GB',
     memory: '4096',
-    ip: '10.0.10.10',
+    ip: '10.0.10.20',
     hostname: 'ukm.dev'
 }
